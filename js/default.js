53a25d8ca917d6423ec71cc9d2b4860ae1bd8c2d
Ming <ming.z820620@gmail.com>
2019-08-05 23:20:43 +0800

    changelog: 增加 register 頁面

--- js/default.js
+++ js/default.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
     $('.option').hover(function(event) {
         /* Stuff to do when the mouse enters the element */
-        
+
         $(this).toggleClass('active');
 
         /* Stuff to do when the mouse leaves the element */
     });
-    $('.burger').on('click',function(event){
-    	event.preventDefault();
-    	$('body').toggleClass('show');
+    $('.burger').on('click', function(event) {
+        event.preventDefault();
+        $('body').toggleClass('show');
     });
+   
 });
