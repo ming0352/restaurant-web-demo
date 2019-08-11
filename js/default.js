7a13f65ce0cc346ea7da06602482fc633202f0f8
Ming <ming.z820620@gmail.com>
2019-08-11 13:37:47 +0800

    changelog: 1.新增email和password欄位認證 2.新增icon-match圖片

--- js/default.js
+++ js/default.js
@@ -10,5 +10,57 @@ $(document).ready(function() {
         event.preventDefault();
         $('body').toggleClass('show');
     });
-   
+    $('#email').on('change', '.selector', function(event) {
+        event.preventDefault();
+        /* Act on the event */
+    });
+    $(document).on('val', '#pas', function(event) {
+        event.preventDefault();
+        /* Act on the event */
+    });
+    $("#pas-ch,#pas").change(function(event) {
+        var word1 = $("#pas").val();
+        var word2 = $("#pas-ch").val();
+
+
+        if (word1 != word2) {
+            $('.match-icon').removeClass('active');
+            alert("兩次密碼不一樣");
+        } else {
+            $('.match-icon').addClass('active');
+        }
+
+    });
+    $("body").on("change", "#email", function() {
+
+        $Emailchecking = IsEmail($("#email").val());
+
+
+        if ($Emailchecking == false) {
+
+            alert("請填寫正確的E-MAIL格式");
+
+            $("#email").blur(); //離開焦點
+
+        }
+
+    })
+
+    function IsEmail(email) {
+
+        var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+        if (!regex.test(email)) {
+
+            return false;
+
+        } else {
+
+            return true;
+
+        }
+
+    }
+
+
 });
